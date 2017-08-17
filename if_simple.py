e04dc6e69b19899040d46c031f34a15b2dfbcbd4
tangxin <961577196@qq.com>
2017-08-17 21:27:44 +0800

    如何添加控制台参数

--- if_simple.py
+++ if_simple.py
@@ -3,17 +3,21 @@ __author__ = 'clevertang'
 #学习如何控制台调用程序，以及增加参数
 import sys
 
-params = None
-if len(sys.argv) > 1:
-    params = int(sys.argv[1])
-if params is None:
-    print("alert")
-    print("the param is not set")
-
-elif params <-10:
-    print("the param is small")
-
-elif params>10:
-    print("the param is big")
-else:
-    print("the param is middle")
+# params = None
+# if len(sys.argv) ==3:
+#     params = int(sys.argv[1])
+# if params is None:
+#     print("alert")
+#     print("the param is not set")
+#
+# elif params <-10:
+#     print("the param is small")
+#
+# elif params>10:
+#     print("the param is big")
+# else:
+#     print("the param is middle")
+import sys
+print ("脚本名：", sys.argv[0])
+for i in range(1, len(sys.argv)):
+    print ("参数", i, sys.argv[i])
